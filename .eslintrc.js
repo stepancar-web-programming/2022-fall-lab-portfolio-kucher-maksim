636c55680a3343dea79d5b6793c2eed79deaef13
maximka65 <maxim652002@gmail.com>
2022-11-26 14:50:56 +0300

    12

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    commonjs: true,
+    es2021: true
   },
   extends: 'airbnb-base',
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: 'latest'
   },
   rules: {
-    'linebreak-style': 0,
+    'linebreak-style': process.env.NODE_ENV === 'prod' ? 'unix' : 'windows',
     'no-param-reassign': 0,
     'no-unused-expressions': 0,
     'no-unused-vars': 0,
-    'camelcase': 0,
+    camelcase: 0,
     'no-undef': 0,
-    'semi': 0,
+    semi: 0,
     'no-plusplus': 0,
     'func-names': 0,
     'no-console': 0,
     'default-case': 0,
-    'no-multi-assign': 0,
-  },
-};
+    'no-multi-assign': 0
+  }
+}
--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,114 +1,132 @@
-jQuery(document).ready(($) => {
+jQuery(document).ready($ => {
   $('form.contactForm').submit(function () {
-    const f = $(this).find('.form-group');
-    let ferror = false;
-    const emailExp = /^[^\s()<>@,;:/]+@\w[\w.-]+\.[a-z]{2,}$/i;
+    const f = $(this).find('.form-group')
+    let fError = false
+    const emailExp = /^[^\s()<>@,;:/]+@\w[\w.-]+\.[a-z]{2,}$/i
 
     f.children('input').each(function () {
-      const i = $(this);
-      let rule = i.attr('data-rule');
+      const i = $(this)
+      let contactRule = i.attr('data-rule')
 
-      if (rule !== undefined) {
-        let ierror = false;
-        const pos = rule.indexOf(':', 0);
+      if (contactRule !== undefined) {
+        let iError = false
+        const pos = contactRule.indexOf(':', 0)
         if (pos >= 0) {
-          const exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
+          const exp = contactRule.substr(pos + 1, contactRule.length)
+          contactRule = contactRule.substr(0, pos)
         } else {
-          rule = rule.substr(pos + 1, rule.length);
+          contactRule = contactRule.substr(pos + 1, contactRule.length)
         }
 
-        switch (rule) {
+        switch (contactRule) {
           case 'required':
             if (i.val() === '') {
-              ferror = ierror = true;
+              fError = iError = true
             }
-            break;
+            break
 
           case 'minlen':
             if (i.val().length < parseInt(exp, 10)) {
-              ferror = ierror = true;
+              fError = iError = true
             }
-            break;
+            break
 
           case 'email':
             if (!emailExp.test(i.val())) {
-              ferror = ierror = true;
+              fError = iError = true
             }
-            break;
+            break
 
           case 'checked':
             if (!i.is(':checked')) {
-              ferror = ierror = true;
+              fError = iError = true
             }
-            break;
+            break
 
           case 'regexp':
-            exp = new RegExp(exp);
+            exp = new RegExp(exp)
             if (!exp.test(i.val())) {
-              ferror = ierror = true;
+              fError = iError = true
             }
-            break;
+            break
         }
         // eslint-disable-next-line no-nested-ternary
-        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        i.next('.validation')
+          .html(
+            iError
+              ? i.attr('data-msg') !== undefined
+                ? i.attr('data-msg')
+                : 'wrong Input'
+              : ''
+          )
+          .show('blind')
       }
-    });
+    })
     f.children('textarea').each(function () {
-      const i = $(this);
-      let rule = i.attr('data-rule');
+      const i = $(this)
+      let contactRule = i.attr('data-rule')
 
-      if (rule !== undefined) {
-        let ierror = false;
-        const pos = rule.indexOf(':', 0);
+      if (contactRule !== undefined) {
+        let iError = false
+        const pos = contactRule.indexOf(':', 0)
         if (pos >= 0) {
-          const exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
+          const exp = contactRule.substr(pos + 1, contactRule.length)
+          contactRule = contactRule.substr(0, pos)
         } else {
-          rule = rule.substr(pos + 1, rule.length);
+          contactRule = contactRule.substr(pos + 1, contactRule.length)
         }
 
-        switch (rule) {
+        switch (contactRule) {
           case 'required':
             if (i.val() === '') {
-              ferror = ierror = true;
+              fError = iError = true
             }
-            break;
+            break
 
           case 'minlen':
             // eslint-disable-next-line block-scoped-var
             if (i.val().length < parseInt(exp, 10)) {
-              ferror = ierror = true;
+              fError = iError = true
             }
-            break;
+            break
         }
         // eslint-disable-next-line no-nested-ternary
-        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        i.next('.validation')
+          .html(
+            iError
+              ? i.attr('data-msg') !== undefined
+                ? i.attr('data-msg')
+                : 'wrong Input'
+              : ''
+          )
+          .show('blind')
       }
-    });
+    })
 
-    if (ferror) return false;
-    const str = $(this).serialize();
-    let action = $(this).attr('action');
-    if (!action) {
-      action = 'contactform/contactform.php';
+    if (fError) {
+      return false
+    }
+    const str = $(this).serialize()
+    let contactAction = $(this).attr('action')
+    if (!contactAction) {
+      contactAction = 'contactform/contactform.php'
     }
     $.ajax({
       type: 'POST',
-      url: action,
+      url: contactAction,
       data: str,
-      success(msg) {
+      success (msg) {
         if (msg === 'OK') {
-          $('#sendmessage').addClass('show');
-          $('#errormessage').removeClass('show');
-          $('.contactForm').find('input, textarea').val('');
+          $('#sendmessage').addClass('show')
+          $('#errormessage').removeClass('show')
+          $('.contactForm').find('input, textarea').val('')
         } else {
-          $('#sendmessage').removeClass('show');
-          $('#errormessage').addClass('show');
-          $('#errormessage').html(msg);
+          $('#sendmessage').removeClass('show')
+          $('#errormessage').addClass('show')
+          $('#errormessage').html(msg)
         }
-      },
-    });
-    return false;
-  });
-});
+      }
+    })
+    return false
+  })
+})
